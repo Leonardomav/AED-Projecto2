4037b05ab4008294fcaf6b4554397ed37bd2828c
jmmonteiro <jmonteiro@student.dei.uc.pt>
2017-05-20 01:39:00 +0100

    GUI interface pretty much done

--- gui.py
+++ gui.py
@@ -17,17 +17,17 @@ def on_entry_click_1(event):				#Cenario Entry
 		entry_cenario.delete(0, "end")		#delete all the text in the entry
 		entry_cenario.insert(0, '')			#Insert blank for user input
 
-def on_entry_click_2(event):				#Min Distance Entry
-	entry_min_dist.config(fg = 'black')
-	if entry_min_dist.get() == 'Min dist':
-		entry_min_dist.delete(0, "end")		#delete all the text in the entry
-		entry_min_dist.insert(0, '')		#Insert blank for user input
+# def on_entry_click_2(event):				#Min Distance Entry
+# 	entry_min_dist.config(fg = 'black')
+# 	if entry_min_dist.get() == 'Min dist':
+# 		entry_min_dist.delete(0, "end")		#delete all the text in the entry
+# 		entry_min_dist.insert(0, '')		#Insert blank for user input
 
-def on_entry_click_3(event):				#Max Distance Entry
-	entry_max_dist.config(fg = 'black')
-	if entry_max_dist.get() == 'Max dist':
-		entry_max_dist.delete(0, "end")		#delete all the text in the entry
-		entry_max_dist.insert(0, '')		#Insert blank for user input
+# def on_entry_click_3(event):				#Max Distance Entry
+# 	entry_max_dist.config(fg = 'black')
+# 	if entry_max_dist.get() == 'Max dist':
+# 		entry_max_dist.delete(0, "end")		#delete all the text in the entry
+# 		entry_max_dist.insert(0, '')		#Insert blank for user input
 
 
 def on_focusout_0(event):				#Cities Entry
@@ -46,21 +46,21 @@ def on_focusout_1(event):				#Cenario Entry
 	elif not is_int(entry_cenario.get()):	#If the input isn't valid, the text goes red
 		entry_cenario.config(fg = 'red')
 
-def on_focusout_2(event):				#Min Distance Entry
-	if entry_min_dist.get() == '':
-		entry_min_dist.insert(0, 'Min dist')
-		entry_min_dist.config(fg = 'grey')
+# def on_focusout_2(event):				#Min Distance Entry
+# 	if entry_min_dist.get() == '':
+# 		entry_min_dist.insert(0, 'Min dist')
+# 		entry_min_dist.config(fg = 'grey')
 	
-	elif not is_int(entry_min_dist.get()):	#If the input isn't valid, the text goes red
-		entry_min_dist.config(fg = 'red')
+# 	elif not is_int(entry_min_dist.get()):	#If the input isn't valid, the text goes red
+# 		entry_min_dist.config(fg = 'red')
 
-def on_focusout_3(event):				#Max Distance Entry
-	if entry_max_dist.get() == '':
-		entry_max_dist.insert(0, 'Max dist')
-		entry_max_dist.config(fg = 'grey')
+# def on_focusout_3(event):				#Max Distance Entry
+# 	if entry_max_dist.get() == '':
+# 		entry_max_dist.insert(0, 'Max dist')
+# 		entry_max_dist.config(fg = 'grey')
 
-	elif not is_int(entry_max_dist.get()):	#If the input isn't valid, the text goes red
-		entry_max_dist.config(fg = 'red')
+# 	elif not is_int(entry_max_dist.get()):	#If the input isn't valid, the text goes red
+# 		entry_max_dist.config(fg = 'red')
 
 
 ########## CHECKBOX STUFF ###########
@@ -83,7 +83,7 @@ def on_check_cenario_2():
 # 	brute_opt_check.deselect()
 
 
-########## THAT PART OF THE CODE THAT USES THE OTHER .py FILES AND DOES SOMETHING PRODUCTIVE ##########
+#J#M#S#M#o#n#t#e#i#r#o# THAT PART OF THE CODE THAT USES THE OTHER .py FILES AND DOES SOMETHING PRODUCTIVE #J#M#S#M#o#n#t#e#i#r#o#
 def is_int(string):
 	try:
 		int(string)
@@ -95,13 +95,12 @@ def check_values():
 	cities = is_int(entry_cities.get())
 	#cenario = is_int(entry_cenario.get())	#converted into checkboxes
 	cenario = (cenario_1.get() or cenario_2.get())
-	min_dist = is_int(entry_min_dist.get())
-	max_dist =is_int(entry_max_dist.get())
+	#min_dist = is_int(entry_min_dist.get())
+	#max_dist =is_int(entry_max_dist.get())
 
-	if (cities and cenario and min_dist and max_dist) and int(entry_min_dist.get()) < int(entry_max_dist.get()):
+	if cities and cenario:
 		#do execute the program
-		print('hello')
-		print(str(cenario_1.get()) + "   " + str(cenario_2.get()))
+		run()
 
 	else:
 		if not cities:
@@ -110,18 +109,56 @@ def check_values():
 		if not cenario:
 			messagebox.showerror("Invalid Cenario",		"Please select one of the cenarios")
 
-		if not min_dist:
-			messagebox.showerror("Invalid Distance",	"The value of 'Minimum city distance' is not valid!")
+		# if not min_dist:
+		# 	messagebox.showerror("Invalid Distance",	"The value of 'Minimum city distance' is not valid!")
 
-		if not max_dist:
-			messagebox.showerror("Invalid Distance",	"The value of 'Maximum city distance' is not valid!")
+		# if not max_dist:
+		# # 	messagebox.showerror("Invalid Distance",	"The value of 'Maximum city distance' is not valid!")
 
-		if min_dist and max_dist:
-			if not int(entry_min_dist.get()) < int(entry_max_dist.get()):
-				messagebox.showerror("Invalid values", "The 'Minimum city distance' value should be lower than the 'Maximum city distance'!")			
+		# if min_dist and max_dist:
+		# 	if not int(entry_min_dist.get()) < int(entry_max_dist.get()):
+		# 		messagebox.showerror("Invalid values",	"The 'Minimum city distance' value should be lower than the 'Maximum city distance'!")			
+
+def run():
+	###Popup Window with results###
+	pop_up = Toplevel()
+	pop_up.title("Results")
+
+	#Cenas do leo
+	grafo = Message(pop_up, text="CONACONACONACONACONACONACONACONACONACONACONACONACONACONACONACONACONACONACONACONACONACONA o homem ra", width=400)	#Mess arround with the width parameter to fit all your needs
+	grafo.grid(row=0, columnspan=4, pady=10, padx=10)
 
 
-########## ACTUAL TKINTER'S CODE STARTS HERE ##########
+	#Pop-up labels
+	label_origin = Label(pop_up,	text="City of origin: ")
+	label_total_dist = Label(pop_up,text="Total distance: ")
+	label_path = Label(pop_up,		text="Route chosen: ")
+	label_time = Label(pop_up,		text="Calculated in: ")
+
+	#Pop-up info to be displayed
+	origin_info = Message(pop_up, 		width=300, text="A")
+	total_dist_info = Message(pop_up, 	width=300, text="50")
+	path_info = Message(pop_up, 		width=300, text="A - B - D - C - E - A")
+	time_info = Message(pop_up, 		width=300, text="0.000005459123")
+
+	#Grid it!
+	label_origin.grid(row=10,	pady=10, sticky=E)
+	label_total_dist.grid(row=12,pady=10, sticky=E)
+	label_path.grid(row=14,		pady=10, sticky=E)
+	label_time.grid(row=16,		pady=10, sticky=E)
+
+	origin_info.grid(row=10,		column=2, pady=10, sticky=W)
+	total_dist_info.grid(row=12,	column=2, pady=10, sticky=W)
+	path_info.grid(row=14,		column=2, pady=10, sticky=W)
+	time_info.grid(row=16,		column=2, pady=10, sticky=W)
+	
+
+	btn = Button(pop_up, text="Dismiss", command=pop_up.destroy)
+	btn.grid(row=100,	pady=10, column=1, sticky=W)
+
+
+########## ACTUAL TKINTER'S CODE STARTS HERE (Most of it anyways)##########
+###Main Window with parameter and such###
 root = Tk()		#does stuff
 root.wm_title("AED - Project #2")	#Window Name
 
@@ -129,7 +166,7 @@ top_frame = Frame(root)		#Creates top frame
 top_frame.pack()			#Packs the frame in the window
 
 bottom_frame = Frame(root)		#Creates bottom frame
-bottom_frame.pack(side=BOTTOM)	#Packs the frame in the window. undet the other frame
+bottom_frame.pack(side=BOTTOM)	#Packs the frame in the window. under the other frame
 
 calculate_btn = Button(bottom_frame,text="Calculate",	fg="green", command=check_values) 	#button to start program
 exit_btn = Button(bottom_frame,		text="Exit",		fg="green", command=root.quit)		#button to exit the GUI
@@ -140,9 +177,9 @@ exit_btn.pack(side=LEFT)		#places the button as far to the left as it can
 
 #Grid for inputs and stuff
 #sticky -> [N,S,W,E]
-label_cidades = Label(top_frame,	text="Number of cities:   ")
+label_cities = Label(top_frame,		text="Number of cities:   ")
 label_cenario = Label(top_frame,	text="Cenario:   ")
-label_dist_cid = Label(top_frame,	text="Distance between cities:   ")
+#label_dist_cid = Label(top_frame,	text="Distance between cities:   ")
 label_algorithm = Label(top_frame,	text="Algorithm to be used:   ")
 label_filler = Label(top_frame,		text="")
 
@@ -157,8 +194,8 @@ entry_max_dist = Entry(top_frame,	fg='grey')
 #entry boxes default text
 entry_cities.insert(0,		'# of cities')
 #entry_cenario.insert(0,		'Cenario 1 or 2')	#converted into checkboxes
-entry_min_dist.insert(0,	'Min dist')
-entry_max_dist.insert(0,	'Max dist')
+#entry_min_dist.insert(0,	'Min dist')
+#entry_max_dist.insert(0,	'Max dist')
 
 #Var for checkboxes
 cenario_1 = BooleanVar()
@@ -178,19 +215,19 @@ recursive_check = Checkbutton(top_frame,	text="Recursive ",				onvalue=True, off
 #binds
 entry_cities.bind('<FocusIn>',		on_entry_click_0)
 #entry_cenario.bind('<FocusIn>',	on_entry_click_1)		#converted into checkboxes
-entry_min_dist.bind('<FocusIn>',	on_entry_click_2)
-entry_max_dist.bind('<FocusIn>',	on_entry_click_3)
+#entry_min_dist.bind('<FocusIn>',	on_entry_click_2)
+#entry_max_dist.bind('<FocusIn>',	on_entry_click_3)
 
 entry_cities.bind('<FocusOut>',		on_focusout_0)
 #entry_cenario.bind('<FocusOut>',	on_focusout_1)			#converted into checkboxes
-entry_min_dist.bind('<FocusOut>',	on_focusout_2)
-entry_max_dist.bind('<FocusOut>',	on_focusout_3)
+#entry_min_dist.bind('<FocusOut>',	on_focusout_2)
+#entry_max_dist.bind('<FocusOut>',	on_focusout_3)
 
 
 #grid labels
-label_cidades.grid(row=0,	pady=10,	sticky=E)
+label_cities.grid(row=0,	pady=10,	sticky=E)
 label_cenario.grid(row=2,	pady=10,	sticky=E)
-label_dist_cid.grid(row=4,	pady=10,	sticky=E)
+#label_dist_cid.grid(row=4,	pady=10,	sticky=E)
 label_algorithm.grid(row=6,	pady=10,	sticky=E)
 label_filler.grid(row=10,	pady=10,	sticky=E)
 
@@ -198,8 +235,8 @@ label_filler.grid(row=10,	pady=10,	sticky=E)
 #grid Entries
 entry_cities.grid(row=0,	column=1)
 #entry_cenario.grid(row=2,	column=1)		#converted into checkboxes
-entry_min_dist.grid(row=4,	column=1)
-entry_max_dist.grid(row=4,	column=2)
+#entry_min_dist.grid(row=4,	column=1)
+#entry_max_dist.grid(row=4,	column=2)
 
 
 #grid checkboxes
